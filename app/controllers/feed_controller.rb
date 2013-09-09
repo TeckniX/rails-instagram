11adc4ecb00f82eb1131380166dcad818a586bfd
TeckniX <lokitek@gmail.com>
2013-09-09 14:50:16 -0400

    Added initial info

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -4,6 +4,11 @@ class FeedController < ApplicationController
 
     client = Instagram.client(:access_token => session[:access_token])
     @user = client.user
-    @recent_media_items = client.user_recent_media
+    ## 777 is the user's ID - to be obtained via? 
+    ## https://github.com/zeantsoi/rails-instagram-uid/blob/master/app/controllers/lookup_controller.rb
+    #users = client.user_search('marissa_ang')
+    #@recent_media_items = client.user_recent_media(users[0].id)
+    @recent_media_items = client.tag_recent_media('skullcandysnow', :no_response_wrapper => true)
+    #@recent_media_items = client.media_popular(:no_response_wrapper => true)
   end
 end
--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,8 +1,8 @@
 require "instagram"
 
 Instagram.configure do |config|
-  config.client_id = "CLIENT_ID"
-  config.client_secret = "CLIENT_SECRET"
+  config.client_id = "f4e9d86cb1ac40a89a45adddb7909d90"
+  config.client_secret = "ed71381f67bd4ea0aefbd4b3a4422d57"
 end
 
-CALLBACK_URL = "http://localhost:3000/session/callback"
+CALLBACK_URL = "http://rails-cron-3062.use1.actionbox.io:3000/session/callback"
