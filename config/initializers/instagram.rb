5888ba221a072dbb777c6b54877d3565db78eb4b
TeckniX <lokitek@gmail.com>
2013-09-09 14:52:32 -0400

    Removed original instagram file

--- config/initializers/instagram.rb
+++
@@ -1,8 +0,0 @@
-require "instagram"
-
-Instagram.configure do |config|
-  config.client_id = "f4e9d86cb1ac40a89a45adddb7909d90"
-  config.client_secret = "ed71381f67bd4ea0aefbd4b3a4422d57"
-end
-
-CALLBACK_URL = "http://rails-cron-3062.use1.actionbox.io:3000/session/callback"
